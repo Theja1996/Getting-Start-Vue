ad90faeb58a1ce8a24cdce7b80000640895a254d
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-10-28 18:19:57 +0530

    component

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>hello vue home</h1>
+    <h1>{{data}}</h1>
+      <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    props:{
+        data: String,
+        msg:String
+    }
+}
+</script>
