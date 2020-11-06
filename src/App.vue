8342dc63bedbd552e0d4e1e87cbd7b4ed9bc7275
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-11-06 19:49:48 +0530

    databinding

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,24 @@
  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
  <!-- <Welcome />-->
 <!-- <Temp /> -->
-<Deco />
-
+<!--<Deco /> -->
+<Databind />
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 //import Welcome from './components/welcome.vue'
 //import Temp from './components/template.vue'
-import Deco from './components/style.vue'
-
+//import Deco from './components/style.vue'
+import Databind from './components/Databind.vue'
 export default {
   name: 'App',
   components: {
   //  HelloWorld
  // Welcome
  //Temp
-   Deco
+  // Deco
+  Databind
   }
 }
 </script>
---
+++ src/components/Databind.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>hi with databinding</h1>
+  <h1>{{ message }}</h1>
+  <input type="text" v-model="message">
+
+  <h1 style="white-space:pre-line">{{  clip }}</h1>
+  <textarea type="text" v-model=" clip" />
+</template>
+
+<script>
+export default {
+    name:'Databind',
+    data(){
+        return{
+            message:"hi ",
+            clip:"hello"
+        }
+    }
+}
+</script>
